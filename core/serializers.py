b4f603130e873c37a24a52221c6647519a931d2a
Marco Torres <marcoantonio.torres@icloud.com>
2023-12-16 20:59:20 -0500

    I created the serializers.py file where it is used to allow complex data such as querysets and model instances to be converted to native Python datatypes that can be easily rendered into JSON or other content types. In this case we use it to retrieve the User data in our views.py. Once this is done we can work with the data in our views using the request.data object (this being another form of request.POST and request.FILES. However, it comes with additional resources). We then assign that object to a variable where we implement validation. If the paswords do not match then we throw an exception, the rest of the validation should be handled by the User Serializer. We then check if the fields we asked for in our serializers are provided, if not then raise exception is = to True. We then save the user and return the Response in JSON format

--- core/serializers.py
+++ core/serializers.py
@@ -1,5 +1,17 @@
-
+# To have access to ModelSerializer
 from rest_framework.serializers import ModelSerializer
+
+from rest_framework.views import Response
 # To have access to our User class
 from .models import User
+
+# What are Serializers and why do we need them?
+# When we create the user, we return the User data in our views.py
+# But we can not return the User as an object as it is (it will throw an error (meaning we have to serialize the object))
+# That object is converted to JSON (this is done through the serializers)
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        # We can also use '__all__' however, this will return all the fields in core_users
+        fields = ['id', 'first_name', 'last_name', 'email', 'password']
 
--- core/views.py
+++ core/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+
+# To handle exception from the REST framework
+from rest_framework import exceptions
+
+# APIView to return the request as a view
 from rest_framework.views import APIView
 
 # Find out what the code below is doing
 from rest_framework.response import Response
+
+from .serializers import UserSerializer
 
 # Create your views here.
 # Create view using the API view (extends from API view)
@@ -10,6 +17,25 @@ from rest_framework.response import Response
 # To add to our route we add it to urls.py
 class RegisterAPIView(APIView):
     def post(self, request):
+
+        # Returning the content of the request body for validation (specifically the password)
+        data = request.data
+
+        # Validation on data for password
+        if data['password'] != data['password_confirm']:
+            # The other validations should be handles by the User Serializer
+            raise exceptions.APIException('Passwords do not match!')
+        
+        # data = data is required because None may be a valid input or output value
+        serializer = UserSerializer(data=data)
+        # If one of our fields is not appearing, then we raise an exception
+        serializer.is_valid(raise_exception=True)
+        # We save it (essentially creating our user from this)
+        serializer.save()
+        # Here we return it in a JSON format
+        return Response(serializer.data)
+
         # After this we can test in Postman (this is all posible after the pathing is completed
         # (urls.py in app -> urls.py in core -> views.py ))
-        return Response(request.data)
+        # return Response(request.data)
+        
