038c8e8ec6db33b0a17592203e4905ca6e6a3b40
Marco Torres <marcoantonio.torres@icloud.com>
2023-12-18 14:06:35 -0500

    Created a post function that allows us to register a User. We use serializers to provide a way for python to understands the objects that are coming in. I then added some authentication logic using exceptions.AuthenticationFailed. After the authentication is completed we return the response and create the view using APIView. I also installed jwt, which is the industy standard method for representing claims securely between two parties.

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 # When we hit the api path (located in app folder under urls.py) we get taken to this urls.py unser core folder app
 from django.urls import path
-from .views import RegisterAPIView
+from .views import RegisterAPIView, LoginAPIView
 
 urlpatterns = [
     # In here we need to add the path (register)
     # When we hist the register path we call it as a view
     path('register', RegisterAPIView.as_view()),  # FIRST PROB I didn't correctly add the pathing
+    path('login', LoginAPIView.as_view()),
 ]
--- core/views.py
+++ core/views.py
@@ -11,6 +11,9 @@ from rest_framework.response import Response
 
 from .serializers import UserSerializer
 
+# Import to access the contents in models
+from .models import User
+
 # Create your views here.
 # Create view using the API view (extends from API view)
 # This is the base class when we create an end point
@@ -38,4 +41,36 @@ class RegisterAPIView(APIView):
         # After this we can test in Postman (this is all posible after the pathing is completed
         # (urls.py in app -> urls.py in core -> views.py ))
         # return Response(request.data)
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        # .data reminds of the object we pass from our template to our component when trying to access different objects in the DOM
+        email = request.data['email']
+        password = request.data['password']
+
+        # Filter where email is equal to email, and get the first value since our code doesn't know that the email is unique
+        user = User.objects.filter(email=email).first()
+
+        # IMPORTANT to raise both exceptions because the user doesn't know if he put the wrong email or the wrong password
+
+        # If we did not find the user 
+        if user is None:
+            # raise AuthFailed exception and let the user know (email)
+            raise exceptions.AuthenticationFailed('Invalid Email')
+
+        # ISSUE BELOW, (WHEN I CHECK TO GET THE BOOLEAN IT IS NOT CORRECT)
+        # If the email is correct, now we check the password (We use a built in function check_password)
+        if not user.check_password(password):
+            raise exceptions.AuthenticationFailed(f"password: {password}")
         
+        if password != user.password:
+            raise exceptions.AuthenticationFailed("Invalid Password")
+        
+        # We serialize the User (create)
+        serializer = UserSerializer(user)
+
+        # We return the data so we can see it
+        return Response(serializer.data)
+ 
+
+    
